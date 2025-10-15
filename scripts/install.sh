a4ec1d8762efcdc3ffa7bcd8562d0fa3d126eb89
Jean-Christophe Aude <jean-christophe.aude@cea.fr>
2025-10-15 18:29:35 +0200

    NEW add path & branch args

--- scripts/install.sh
+++ scripts/install.sh
@@ -10,23 +10,45 @@ OK() {
   echo -e "${green}OK${norm}"
 }
 
+Error() {
+    [ $# -eq 2 ] && echo -e "$2"
+    echo -e "${bred}ERROR: ${red}$1${norm}"
+}
+
+
+INSTALL_PATH="${HOME}/.tgcc"
+TGCC_BRANCH="main"
+
+OPTSTRING=":p:b:"
+hile getopts ${OPTSTRING} opt; do
+  case "$opt" in
+  p) INSTALL_PATH=$OPTARG
+      ;;
+  b) TGCC_BRANCH=$OPTARG
+      ;;
+  *) Error "Unknown Arguments.. exit"
+      exit 1
+      ;;
+  esac
+done
+
 echo -e "${bold}[ Deploy InstallerTGCC on your computer ]${norm}"
 
 echo -n "- create local '~/bin' directory "
 mkdir -p ${HOME}/bin
 OK
 
-echo -n "- create local installation folder '~/.tgcc' "
-mkdir -p ${HOME}/.tgcc
-cd ${HOME}/.tgcc
+echo -n "- create local installation folder '${INSTALL_PATH}' "
+mkdir -p ${INSTALL_PATH}
+cd ${INSTALL_PATH}
 OK
 
 echo  "- clone repository.."
-git clone git@github.com:neurospin-brainomics/InstallerTGCC.git
+git clone -b $TGCC_BRANCH git@github.com:neurospin-brainomics/InstallerTGCC.git
 
 echo -n "- link InstallerTGCC files in the bin directory "
-ln -sf ~/.tgcc/InstallerTGCC/docker/scripts/push-* ~/bin
-ln -sf ~/.tgcc/InstallerTGCC/sync/scripts/sync-* ~/bin
+ln -sf "${INSTALL_PATH}/InstallerTGCC/docker/scripts/push-*" "${HOME}/bin"
+ln -sf "${INSTALL_PATH}/InstallerTGCC/sync/scripts/sync-*" "${HOME}/bin"
 OK
 
 echo ""
@@ -39,7 +61,7 @@ echo -e "${bold}[ Deploy InstallerTGCC on TGCC ]${norm}"
 
 read -p "> Enter your TGCC ssh configuration profile (or ENTER if you haven't configure SSH profiles): " TGCC_SSH_CONFIG
 if [[ -z "${TGCC_SSH_CONFIG}" ]]; then
-  echo -e "${red}Abort remote installation${norm}"
+  Error "Abort remote installation"
   exit 0
 fi
 
@@ -49,35 +71,35 @@ ssh -q $TGCC_SSH_CONFIG -t $TGCC_CMD
 if [ $? -eq 0 ]; then
   OK
 else
-  echo -e "\n${err}Fail to create TGCC '~/bin' folder (rc=$?).. abort${norm}"
+  Error "Fail to create TGCC '~/bin' folder (rc=$?).. abort" "\n"
   exit 10
 fi
 
-echo -n "- create TGCC installation folder '~/.tgcc' "
-TGCC_CMD="bash -c \"mkdir -p ~/.tgcc\""
+echo -n "- create TGCC installation folder '${TGCC_INSTALL}' "
+TGCC_CMD="bash -c \"mkdir -p $TGCC_INSTALL\""
 ssh -q $TGCC_SSH_CONFIG -t $TGCC_CMD
 if [ $? -eq 0 ]; then
   OK
 else
-  echo -e "\n${err}Fail to create installation folder on TGCC (rc=$?).. abort${norm}"
+  Error "Fail to create installation folder on TGCC (rc=$?).. abort" "\n"
   exit 10
 fi
 
 echo "- synchronise InstallerTGCC repository"
 echo ""
-$HOME/bin/sync-tgcc.sh -s "$HOME/.tgcc/InstallerTGCC" -d ".tgcc/" -z
+$HOME/bin/sync-tgcc.sh -s "$HOME/.tgcc/InstallerTGCC" -d "${TGCC_INSTALL}" -z
 if [ $? -ne 0 ]; then
-  echo -e "\n${err}Synchronisastion fail .. abort${norm}"
+  Error "Synchronisastion fail .. abort" "\n"
   exit 10
 fi
 
 echo -n "- link scripts in ~/bin on TGCC "
-TGCC_CMD="bash -c \"ln -sf ~/.tgcc/InstallerTGCC/docker/scripts/docker-import.sh ~/bin\""
+TGCC_CMD="bash -c \"ln -sf $TGCC_INSTALL/InstallerTGCC/docker/scripts/docker-import.sh ~/bin\""
 ssh -q $TGCC_SSH_CONFIG -t $TGCC_CMD
 if [ $? -eq 0 ]; then
   OK
 else
-  echo -e "\n${err}Fail to link docker import script.. abort${norm}"
+  Error "Fail to link docker import script.. abort${norm}" "\n"
 fi
 
 echo -e "\n${green}[ --- InstallerTGCC setup complete --- ]${norm}"
