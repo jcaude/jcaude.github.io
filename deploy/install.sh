d34537727b38b1bab6408d7d887d2aee8c6ed40e
Jean-Christophe AUDE <jean-christophe.aude@cea.fr>
2025-12-16 14:33:54 +0100

    UPD deploy/install script

--- deploy/install.sh
+++ deploy/install.sh
@@ -35,6 +35,29 @@ ARCH="$(uname -m)"
 echo "- detected OS=${OS} ARCH=${ARCH}"
 
 # download assets
+DL_CMD="curl -fsL -o"
+if [ -z "$(which curl)" ]; then
+    if [ -z "$(which wget)" ]; then
+        Error "Neither curl nor wget is installed. Please install one of these tools to proceed."
+        exit 1
+    else
+        DL_CMD="wget -qO"
+    fi
+fi
+
+echo -n "- download deploy binary.. "
+${DL_CMD} "${HOME}/bin/deploy" "https://jcaude.github.io/deploy/assets/deploy-${OS}-${ARCH}"
+chmod +x "${HOME}/bin/deploy"
+OK
+
+echo -n "- download release notes and warnings.."
+${DL_CMD} "${HOME}/.config/deploy/RELEASE" "https://jcaude.github.io/deploy/assets/RELEASE"
+${DL_CMD} "${HOME}/.config/deploy/WARNINGS" "https://jcaude.github.io/deploy/assets/WARNINGS"
+OK
 
 # auto-configure
+echo -n "- auto-configure deploy.. "
+export RUST_LOG="error"
+$HOME/bin/deploy install
+OK
 
